0aa23b055bda27dbbfe7c4b6ad9c309256f4bc61
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-11-19 01:23:20 -0300

    Tipado en build

---
+++ buildNode.js
@@ -0,0 +1,16 @@
+const esbuild = require('esbuild');
+const esbuildPluginTsc = require('esbuild-plugin-tsc');
+
+console.info('Building Node interpreter...');
+console.time('Done in');
+esbuild.build({
+	entryPoints: [ 'src/index.js' ],
+	outfile: 'dist/puréscript.bundle.node.js',
+	bundle: true,
+	ignoreAnnotations: false,
+	plugins: [
+		esbuildPluginTsc({
+			force: true,
+		}),
+	],
+}).then(_ => console.timeEnd('Done in'));
