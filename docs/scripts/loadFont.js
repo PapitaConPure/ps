a1a92fe99d47cfa80f5eeaf8915602430ce0c360
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-11-22 16:47:36 -0300

    Correcciones varias

--- docs/scripts/loadFont.js
+++
@@ -1,9 +0,0 @@
-function loadFont(el) {
-	el.onload = null;
-	el.onerror = null;
-	el.rel = "stylesheet";
-	document.documentElement.style.setProperty(
-		"--f-outfit",
-		'"Outfit", ui-sans-serif, system-ui, sans-serif',
-	);
-}
