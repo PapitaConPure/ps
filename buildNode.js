2c45060cbf3ff90a12d595deb52d49f1549e5b9b
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-11-21 22:46:46 -0300

    Cambiar proceso de build

--- buildNode.js
+++ buildNode.js
@@ -1,8 +1,32 @@
+const chalk = require('chalk');
 const esbuild = require('esbuild');
 const esbuildPluginTsc = require('esbuild-plugin-tsc');
+const fs = require('fs');
+const { sizeof } = require('sizeof');
 
-console.info('Building Node interpreter...');
-console.time('Done in');
+let startTime;
+
+//Browser
+console.info(chalk.italic.gray('Building browser interpreter...'));
+startTime = performance.now();
+esbuild.build({
+	entryPoints: [ 'src/index.js' ],
+	outfile: 'dist/puréscript.bundle.js',
+	globalName: 'PuréScript',
+	platform: 'browser',
+	bundle: true,
+	minify: true,
+}).then(_ => {
+	const timespan = performance.now() - startTime;
+	const title = chalk.bold.bgYellowBright.black(' Browser '.padEnd(20));
+	const sizeResult = chalk.blueBright(`${sizeof(fs.readFileSync('./dist/puréscript.bundle.js', { encoding: 'utf-8' }), true)} in size`);
+	const timeResult = chalk.greenBright(`Done in ${timespan.toFixed(0)}ms`);
+	console.info(`${title}\n${sizeResult}\n${timeResult}`);
+});
+
+//Node
+console.info(chalk.italic.gray('Building Node interpreter...'));
+startTime = performance.now();
 esbuild.build({
 	entryPoints: [ 'src/index.js' ],
 	outfile: 'dist/puréscript.bundle.node.js',
@@ -14,4 +38,10 @@ esbuild.build({
 			force: true,
 		}),
 	],
-}).then(_ => console.timeEnd('Done in'));
+}).then(_ => {
+	const timespan = performance.now() - startTime;
+	const title = chalk.bold.bgRed.whiteBright(' Node '.padEnd(20));
+	const sizeResult = chalk.blueBright(`${sizeof(fs.readFileSync('./dist/puréscript.bundle.node.js', { encoding: 'utf-8' }), true)} in size`);
+	const timeResult = chalk.greenBright(`Done in ${timespan.toFixed(0)}ms`);
+	console.info(`${title}\n${sizeResult}\n${timeResult}`);
+});
