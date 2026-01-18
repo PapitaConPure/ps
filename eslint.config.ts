af173e1a8401d6ef3d9f93481641dbe48c74b189
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-18 02:09:03 -0300

    trailing comma

--- eslint.config.ts
+++ eslint.config.ts
@@ -26,8 +26,8 @@ export default defineConfig([
           args: 'all',
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
-        }
-      ]
+        },
+      ],
     },
   },
 ]);
