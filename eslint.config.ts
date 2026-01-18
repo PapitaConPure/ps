34d6306e720b67d3a86ce21cc99b6888d5dcf67d
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-18 09:42:31 -0300

    reglas estilo

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,16 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-import stylistic from '@stylistic/eslint-plugin'
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
 	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		files: [ "**/*.{js,ts}" ],
 		plugins: {
 			js,
 			'@stylistic': stylistic
 		},
-		extends: ["js/recommended"],
+		extends: [ "js/recommended" ],
 		languageOptions: {
 			globals: {...globals.browser, ...globals.node}
 		}
@@ -32,56 +32,73 @@ export default defineConfig([
 					varsIgnorePattern: '^_',
 				},
 			],
-			'indent': ['error', 'tab'],
-			'@stylistic/indent': ['error', 'tab'],
+			'indent': [ 'error', 'tab' ],
 			'no-trailing-spaces': 'error',
-			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-      'no-extra-semi': 'error',
-      'semi': ['error', 'always', {
-        'omitLastInOneLineBlock': true,
-        'omitLastInOneLineClassBody': true,
-      }],
-      'semi-spacing': ['error', {'before': false, 'after': true}],
-      'block-spacing': 'error',
-      '@stylistic/block-spacing': 'error',
-      'space-before-blocks': 'error',
-      '@stylistic/space-before-blocks': 'error',
-      '@stylistic/brace-style': ['error', '1tbs', {
-        'allowSingleLine': true,
-      }],
-      'array-bracket-spacing': ['error', 'always'],
-      'space-in-parens': 'error',
-      '@stylistic/keyword-spacing': ['error', {
-        'before': true,
-        'after': false,
-        'overrides': {
-          'else': {
-            'after': true,
-          },
-          'import': {
-            'after': true,
-          },
-          'from': {
-            'after': true,
-          },
-          'as': {
-            'after': true,
-          },
-          'return': {
-            'after': true,
-          },
-          'var': {
-            'after': true,
-          },
-          'let': {
-            'after': true,
-          },
-          'const': {
-            'after': true,
-          },
-        },
-      }],
-      '@stylistic/eol-last': 'error',
-    },
+			'no-mixed-spaces-and-tabs': [ 'error', 'smart-tabs' ],
+			'no-extra-semi': 'error',
+			'semi': [ 'error', 'always', {
+				'omitLastInOneLineBlock': true,
+				'omitLastInOneLineClassBody': true,
+			} ],
+			'semi-spacing': [ 'error', {'before': false, 'after': true} ],
+			'block-spacing': 'error',
+			'@stylistic/block-spacing': 'error',
+			'space-before-blocks': 'error',
+			'@stylistic/space-before-blocks': 'error',
+			'@stylistic/brace-style': [ 'error', '1tbs', {
+				'allowSingleLine': true,
+			} ],
+			'array-bracket-spacing': [ 'error', 'always' ],
+			'space-in-parens': 'error',
+			'@stylistic/keyword-spacing': [ 'error', {
+				'before': true,
+				'after': false,
+				'overrides': {
+					'else': {
+						'after': true,
+					},
+					'import': {
+						'after': true,
+					},
+					'export': {
+						'after': true,
+					},
+					'from': {
+						'after': true,
+					},
+					'as': {
+						'after': true,
+					},
+					'return': {
+						'after': true,
+					},
+					'var': {
+						'after': true,
+					},
+					'let': {
+						'after': true,
+					},
+					'const': {
+						'after': true,
+					},
+					'get': {
+						'after': true,
+					},
+					'try': {
+						'after': true,
+					},
+					'catch': {
+						'after': true,
+					},
+					'finally': {
+						'after': true,
+					},
+					'throw': {
+						'after': true,
+					},
+				},
+			} ],
+			'@stylistic/eol-last': 'error',
+		},
 	},
 ]);
